d2c03296de4348fbe104d112bc24c9b1047c9e0d
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2020-01-17 17:11:02 +0200

    vars

---
+++ 1-variables/script.js
@@ -0,0 +1,33 @@
+// console.log((10 % 6) ** 2);
+let age = 72;
+let hens = 4;
+let roosters = 2;
+let all = hens + roosters;
+
+hens += 2;
+
+let score = 0;
+score = score + 10;
+score += 1;
+score -=100;
+score = 150;
+let bonusMult = 3;
+score *= bonusMult;
+
+let counter = 0;
+counter++;
+counter++;
+counter++;
+counter++;
+counter++;
+counter--;
+
+const year = 1973;
+const pi = 3.14157;
+const peopleCount = 10;
+
+var eggs = 12;
+eggs++;
+eggs = 0;
+
+
